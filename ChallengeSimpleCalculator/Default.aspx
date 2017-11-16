4ecb0a7c0eb9fda4e3e2116ea1323765b255da69
Travis Brooks <tbrooks3141@gmail.com>
2017-11-16 11:13:03 -0800

    Initial commit.

---
+++ ChallengeSimpleCalculator/Default.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ChallengeSimpleCalculator.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <h1>Simple Calculator</h1>
+    
+    </div>
+        <p>
+            First Value:&nbsp;
+            <asp:TextBox ID="number1TextBox" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            Second Value:&nbsp;
+            <asp:TextBox ID="number2TextBox" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Button ID="addButton" runat="server" OnClick="addButton_Click" Text="+" />
+&nbsp;&nbsp;
+            <asp:Button ID="subButton" runat="server" OnClick="subButton_Click" Text="-" />
+&nbsp;&nbsp;
+            <asp:Button ID="multButton" runat="server" OnClick="multButton_Click" Text="x" />
+&nbsp;&nbsp;
+            <asp:Button ID="divButton" runat="server" OnClick="divButton_Click" Text="÷" />
+        &nbsp;&nbsp;
+            <asp:Button ID="exButton" runat="server" Text="X^Y" OnClick="exButton_Click" />
+        </p>
+        <p>
+            <asp:Label ID="answerLabel" runat="server" BackColor="#99CCFF" Font-Size="Large"></asp:Label>
+        </p>
+    </form>
+</body>
+</html>
---
+++ ChallengeSimpleCalculator/Default.aspx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeSimpleCalculator
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void addButton_Click(object sender, EventArgs e)
+        {
+            double number1 = double.Parse(number1TextBox.Text);
+            double number2 = double.Parse(number2TextBox.Text);
+            double sum = number1 + number2;
+            answerLabel.Text = sum.ToString();
+        }
+
+        protected void subButton_Click(object sender, EventArgs e)
+        {
+            double number1 = double.Parse(number1TextBox.Text);
+            double number2 = double.Parse(number2TextBox.Text);
+            double diff = number1 - number2;
+            answerLabel.Text = diff.ToString();
+        }
+
+        protected void multButton_Click(object sender, EventArgs e)
+        {
+            double number1 = double.Parse(number1TextBox.Text);
+            double number2 = double.Parse(number2TextBox.Text);
+            double prod = number1 * number2;
+            answerLabel.Text = prod.ToString();
+        }
+
+        protected void divButton_Click(object sender, EventArgs e)
+        {
+            double number1 = double.Parse(number1TextBox.Text);
+            double number2 = double.Parse(number2TextBox.Text);
+            double quo = number1 / number2;
+            answerLabel.Text = quo.ToString();
+        }
+
+        protected void exButton_Click(object sender, EventArgs e)
+        {
+            double number1 = double.Parse(number1TextBox.Text);
+            double number2 = double.Parse(number2TextBox.Text);
+            if ((number2 % 1) == 0) {
+                double prod = 1;
+                for (int i = 0; i < Math.Abs(number2); i++)
+                {
+                    prod *= number1;
+                }
+                if(number2 < 0) {
+                    prod = 1 / prod;
+                }
+                answerLabel.Text = prod.ToString();
+            } else {
+                answerLabel.Text = "Exponent must be an integer.";
+            }
+        }
+    }
+}
